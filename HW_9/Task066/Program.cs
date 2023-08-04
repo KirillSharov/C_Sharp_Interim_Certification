a591c4310f76bbda8a16878c27b65cf6b4df72aa
kirill.sharov <kirill.scharov@gmail.com>
2023-08-04 23:47:17 +0300

    Added a solution Task66

--- HW_9/Task066/Program.cs
+++ HW_9/Task066/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumBetweenNum(int M, int N)
+{
+    int sum = 0;
+    if (M < N) return sum += M + SumBetweenNum(M + 1, N);
+    else return sum += M;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int M = GetInfo("Введите число M: ");
+int N = GetInfo("Введите число N: ");
+Console.WriteLine($"M = {M}; N = {N} -> {SumBetweenNum(M, N)}");
